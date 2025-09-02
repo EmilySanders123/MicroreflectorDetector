376953db6ee7e4958c5d1b331a27608539d82aff
EmilySanders123 <112529774+EmilySanders123@users.noreply.github.com>
2025-09-02 16:34:30 -0700

    Used OpenCV moments function to detect point centers instead of finding each individual pixel.

--- HighlightReflectors.py
+++ HighlightReflectors.py
@@ -12,21 +12,35 @@ args = parser.parse_args()
 # find image to be processed
 im = cv2.imread(args.path)
 
-# get all points that meet the reflective white threshold
-min_white = [150, 150, 150]
-Y, X = np.where(np.all(im >= min_white, axis=2))
-white_coord_pairs = np.column_stack((X,Y))
-print(white_coord_pairs)
+# mask entire image except for brightest points
+gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+ret, thresh_im = cv2.threshold(gray_im, 125, 255, cv2.THRESH_BINARY)
+# TODO: normalize image colors so brightest color is always pure white?
 
-# make copy of image to write on
-marked_up_image = im.copy()
+# generate contours from filtered image
+contours, hierarchies = cv2.findContours(thresh_im, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-# color white pixels
-for pair in white_coord_pairs:
-    marked_up_image[pair[1], pair[0]] = (5, 5, 255)
+# find set of centerpoints for each contour
+centerpoints = set()
+for i in contours:
+    M = cv2.moments(i)
+    if M['m00'] != 0:
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        centerpoints.add((cx, cy))
+
+# draw all centerpoints on a new image
+im_height, im_width, _ = im.shape
+points_im = np.zeros((im_height, im_width, 3), np.uint8)
+points_im[:,:] = [255, 255, 255]
+for i in centerpoints:
+    cv2.circle(points_im, i, 2, (0, 0, 255), -1)
 
 # display images
 cv2.imshow("Original image", im)
-cv2.imshow("Marked up image", marked_up_image)
+cv2.imshow("Processed image", thresh_im)
+cv2.imshow("Detected points", points_im)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# TODO: make this its own class
