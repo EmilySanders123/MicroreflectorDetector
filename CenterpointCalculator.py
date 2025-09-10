8b1c571d88eb902f098dd9d5b22a725fdc867261
EmilySanders123 <112529774+EmilySanders123@users.noreply.github.com>
2025-09-10 11:11:01 -0700

    Changed some comments and moved points image calculation to display method, so it is done in helper thread.

--- CenterpointCalculator.py
+++ CenterpointCalculator.py
@@ -9,7 +9,13 @@ class CenterpointCalculator:
         pass
 
     def get_centerpoints(self, img_name: str) -> set:
-        # find image to be processed
+        """
+        Finds unique centerpoints of bright points within an image in (x, y) format and returns them as a set.
+        :param img_name: Path to image to be processed
+        :return: Set of unique centerpoints of bright points within an image
+        """
+        
+        # open image to be processed
         im = cv2.imread(img_name)
 
         # mask entire image except for brightest points
@@ -17,7 +23,7 @@ class CenterpointCalculator:
         min_diff = 255 - np.array(gray_im).max()
         gray_im += min_diff
         ret, thresh_im = cv2.threshold(gray_im, 155, 255, cv2.THRESH_BINARY)
-        # TODO: mess with image brightening?
+        # TODO: mess with image brightening values?  change contrast?
 
         # generate contours from filtered image
         contours, hierarchies = cv2.findContours(thresh_im, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -29,15 +35,8 @@ class CenterpointCalculator:
             if point:
                 centerpoints.add(point)
 
-        # draw all centerpoints on a new image
-        im_height, im_width, _ = im.shape
-        points_im = np.zeros((im_height, im_width, 3), np.uint8)
-        points_im[:, :] = [255, 255, 255]
-        for i in centerpoints:
-            cv2.circle(points_im, i, 2, (0, 0, 255), -1)
-
         # display images in separate thread so program doesn't pause until they are closed
-        thread = Thread(target=self.__display_images, args=(im, thresh_im, points_im))
+        thread = Thread(target=self.__display_images, args=(im, thresh_im, centerpoints))
         thread.start()
 
         return centerpoints
@@ -51,9 +50,17 @@ class CenterpointCalculator:
         else:
             return None
 
-    def __display_images(self, og_img: np.ndarray, processed_img: np.ndarray, detected_points: np.ndarray) -> None:
+    def __display_images(self, og_img: np.ndarray, processed_img: np.ndarray, centerpoints: set) -> None:
+        # draw all centerpoints on a new image
+        im_height, im_width, _ = og_img.shape
+        points_img = np.zeros((im_height, im_width, 3), np.uint8)
+        points_img[:, :] = [255, 255, 255]
+        for i in centerpoints:
+            cv2.circle(points_img, i, 2, (0, 0, 255), -1)
+
+        # display images
         cv2.imshow("Original image", og_img)
         cv2.imshow("Processed image", processed_img)
-        cv2.imshow("Detected points", detected_points)
+        cv2.imshow("Detected points", points_img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
