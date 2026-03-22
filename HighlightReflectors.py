50099bb8a232bb70f86b9ec2cf81ee495c7d5e0e
EmilySanders123 <112529774+EmilySanders123@users.noreply.github.com>
2026-03-21 22:46:08 -0700

    Updated storage JSON file name.

--- HighlightReflectors.py
+++ HighlightReflectors.py
@@ -44,7 +44,7 @@ norm_img_centerpoints = list(zip(x_list_normalized, y_list_normalized))
 if args.action.lower() == "store":
     try:
         print("Opening file...")
-        with open("StorageJSON.json", "r+") as file:
+        with open("StorageJSONOldFormat.json", "r+") as file:
             print("File found.  Adding new point data...")
             data = json.load(file)
             id_list = [item.get("id") for item in data["stored_graphs"]]
@@ -62,7 +62,7 @@ if args.action.lower() == "store":
         data = {"stored_graphs": []}
         new_point_obj = {"id": args.id, "points": norm_img_centerpoints}
         data["stored_graphs"].append(new_point_obj)
-        with open("StorageJSON.json", "w") as file:
+        with open("StorageJSONOldFormat.json", "w") as file:
             json.dump(data, file, indent=4)
         print("Point data added.")
     except json.decoder.JSONDecodeError:
@@ -72,7 +72,7 @@ if args.action.lower() == "store":
 elif args.action.lower() == "match":
     try:
         # read data from file
-        with open("StorageJSON.json", "r") as file:
+        with open("StorageJSONOldFormat.json", "r") as file:
             data = json.load(file)
     except FileNotFoundError:
         print("Points file not found.")
@@ -92,7 +92,7 @@ elif args.action.lower() == "match":
 
 elif args.action.lower() == "display":
     try:
-        with open("StorageJSON.json", "r") as file:
+        with open("StorageJSONOldFormat.json", "r") as file:
             data = json.load(file)
     except FileNotFoundError:
         print("Points file not found.")
